17b469a9cb55c8da157c02b098ff1c62436d94de
John Eliasson <john@eliasson.nu>
2024-08-28 20:34:20 +0200

    Created a function that appends the written message from one user to the message board of another user

--- scipt.js
+++ scipt.js
@@ -1,10 +1,11 @@
 const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage(data)
 })
 
 messageForm.addEventListener('submit', e => {
@@ -13,3 +14,9 @@ messageForm.addEventListener('submit', e => {
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
+
+const appendMessage = (message) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
