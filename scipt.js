1144a315c7a376d64a89252a4cf553e1fc40be1a
John Eliasson <john@eliasson.nu>
2024-08-30 09:46:27 +0200

    Formatted everything with prettier

--- scipt.js
+++ scipt.js
@@ -1,36 +1,30 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
-let userName = prompt('What is your name?')
-socket.emit('new-user', userName)
+let userName = prompt("What is your name?");
+socket.emit("new-user", userName);
 
+socket.on("chat-message", (data) => {
+  appendMessage(data.name + ": " + data.message);
+});
 
+socket.on("user-connected", (name) => {
+  appendMessage(name + " connected");
+});
 
-socket.on('chat-message', data => {
-    appendMessage(data)
-})
-
-socket.on('user-connected', name => {
-    appendMessage(name + ' connected')
-})
-
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    socket.emit('send-chat-message', message)
-    messageInput.value = ''
-})
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  socket.emit("send-chat-message", message);
+  messageInput.value = "";
+});
 
 const appendMessage = (message) => {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
-}
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+};
 
-
-
-    appendMessage('Welcome ' + userName)    
-
-
+appendMessage("Welcome " + userName);
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const io = require('socket.io')(3000,{
-    cors: {
-        origin: "*",
-      },
-})
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const users = {}
+const users = {};
 
-
-io.on('connection', socket => {
-    
-socket.on('new-user', userName => {
-    users[socket.id] = userName
-    socket.broadcast.emit('user-connected', userName)
-})
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
-    })
-}) 
+io.on("connection", (socket) => {
+  socket.on("new-user", (userName) => {
+    users[socket.id] = userName;
+    socket.broadcast.emit("user-connected", userName);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
