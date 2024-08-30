c6e7eeef19912c695a3d791857df227fbacac5a4
John Eliasson <john@eliasson.nu>
2024-08-30 16:33:07 +0200

    Made the messege vieable by the sender and also put a 'you' before the message

--- scipt.js
+++ scipt.js
@@ -17,6 +17,7 @@ socket.on("user-connected", (name) => {
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInput.value;
+  appendMessage("You: " + message);
   socket.emit("send-chat-message", message);
   messageInput.value = "";
 });
